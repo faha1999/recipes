90871c1fb7534e87462e8afb4980cd3ec69cc6e6
faha1999 <kafaha1999@gmail.com>
2022-05-06 20:46:48 +0600

    show data from api

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const Popular = () => {
-  return <div>Popular</div>;
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`,
+    );
+    const data = await api.json();
+    setPopular(data.recipes);
+  };
+  return (
+    <div>
+      {popular.map((recipe) => {
+        return (
+          <div key={recipe.id}>
+            <p>{recipe.title}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
