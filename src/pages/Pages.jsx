0135012711eeba79454d9424920b3019e4630648
faha1999 <kafaha1999@gmail.com>
2022-05-09 22:41:46 +0600

    added Category

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Category } from '../components/Category';
 import { Home } from './Home';
 
 export const Pages = () => {
   return (
     <div>
+      <Category />
       <Home />
     </div>
   );
