3661301801350338db632f472a530461c17df1fd
faha1999 <kafaha1999@gmail.com>
2022-05-10 23:34:22 +0600

    completyed Category

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -6,24 +6,29 @@ import { NavLink } from 'react-router-dom';
 export const Category = () => {
   return (
     <div className="Category">
-      <NavLink to={'/cuisine/Italian'}>
+      <NavLink className="nav" to={'/cuisine/Italian'}>
         <FaPizzaSlice />
         <h4>Italian</h4>
       </NavLink>
 
-      <NavLink to={'/cuisine/American'}>
+      <NavLink className="nav" to={'/cuisine/American'}>
         <FaHamburger />
         <h4>American</h4>
       </NavLink>
 
-      <NavLink to={'/cuisine/Thai'}>
+      <NavLink className="nav" to={'/cuisine/Thai'}>
         <GiNoodles />
         <h4>Thai</h4>
       </NavLink>
 
-      <NavLink to={'/cuisine/Japanese'}>
+      <NavLink className="nav" to={'/cuisine/Japanese'}>
         <GiChopsticks />
         <h4>Japanese</h4>
+      </NavLink>
+
+      <NavLink className="nav" to={'/cuisine/Chinese'}>
+        <GiChopsticks />
+        <h4>Chinese</h4>
       </NavLink>
     </div>
   );
