065f98ab7a63befd88c042d821efaf8c35b61d19
faha1999 <kafaha1999@gmail.com>
2022-05-11 00:17:21 +0600

    adeed search jsx

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './css/style.min.css';
 import { Category } from './components/Category';
 import { Pages } from './pages/Pages';
+import { Search } from './components/Search';
 
 function App() {
   return (
     <>
+      <Search />
       <Category />
       <Pages />
     </>
---
+++ src/components/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FaSearch } from 'react-icons/fa';
+
+export const Search = () => {
+  return (
+    <form>
+      <div className="from">
+        <input type="text" />
+      </div>
+    </form>
+  );
+};
