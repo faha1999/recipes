ef7c76ce07e2f7f1b70e92475b6243d6706af481
faha1999 <kafaha1999@gmail.com>
2022-05-11 18:44:29 +0600

    added searched

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 
 export const Search = () => {
+  const [input, setInput] = useState('');
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    navigate('/searched/' + input);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="from">
-        <input type="text" />
+        <FaSearch />
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          value={input}
+        />
       </div>
+      <h1>{input}</h1>
     </form>
   );
 };
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Home } from './Home';
-import { Routes, Route } from 'react-router-dom';
 import { Cuisine } from './Cuisine';
+import { Searched } from './Searched';
+import { Routes, Route } from 'react-router-dom';
 
 export const Pages = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/cuisine/:type" element={<Cuisine />} />
+      <Route path="/Searched/:search" element={<Searched />} />
     </Routes>
   );
 };
