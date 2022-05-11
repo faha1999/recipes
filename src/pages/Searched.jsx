a3d3c88fd2d573d87a0c9856d7237ef48e45b670
faha1999 <kafaha1999@gmail.com>
2022-05-11 19:09:13 +0600

    applied searched function

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 export const Searched = () => {
-  return <div>Searched</div>;
+  const [searchedRecipes, setSearchedRecipes] = useState([]);
+  let params = useParams();
+
+  const getSearched = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}`,
+    );
+    const recipes = await data.json();
+    setSearchedRecipes(recipes.results);
+  };
+
+  useEffect(() => {
+    getSearched(params.search);
+  }, [params.search]);
+
+  return (
+    <div className="grid">
+      {searchedRecipes.map((item) => {
+        return (
+          <div className="card" key={item.id}>
+            <img src={item.image} alt="{item.title}" />
+            <h4>{item.title}</h4>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
