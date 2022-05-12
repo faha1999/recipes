e51535621541e214f82611baaf50262c55750e7c
faha1999 <kafaha1999@gmail.com>
2022-05-12 17:47:12 +0600

    fetch all data

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 export const Recipes = () => {
+  let params = useParams();
+  const [details, setDetails] = useState({});
+
+  const fetchDetails = async () => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`,
+    );
+    const detailsData = await data.json();
+    setDetails(detailsData);
+  };
+
+  useEffect(() => {
+    fetchDetails();
+  }, [params.name]);
+
   return <div>Recipes</div>;
 };
